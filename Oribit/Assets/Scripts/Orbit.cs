b4970e222e8cf6b0138eee2aa97cd434b66d1c62
Sam <114984800+lenchsam@users.noreply.github.com>
2025-08-05 02:05:55 +0100

    Change orbit direction with input

--- Oribit/Assets/Scripts/Orbit.cs
+++ Oribit/Assets/Scripts/Orbit.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class Orbit : MonoBehaviour
 {
     [SerializeField] private Transform _orbitingTarget;
     [SerializeField] private float _orbitSpeed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
         transform.RotateAround(_orbitingTarget.transform.position, Vector3.forward, _orbitSpeed * Time.deltaTime);
+    }
+
+    public void ChangeOrbitDirection()
+    {
+        //putting the orbit speed to negative value will reverse the orbit direction
+        _orbitSpeed = -_orbitSpeed;
     }
 }
